69b00cd7e4bd755a98ca383a54a10d313f61ed63
Chao_Player <1847848758@qq.com>
2025-07-02 13:21:39 +0800

    2025.7.2

--- src/rov_start_pkg/src/rov_init_node.cpp
+++ src/rov_start_pkg/src/rov_init_node.cpp
@@ -14,6 +14,9 @@ void init_callback(const rov_start_pkg::init_msg::ConstPtr& msg){
         status_map[msg->module_name]=true;
         return;
     }
+    else{
+        return;
+    }
 }
 int main(int argc,char **argv){
     ros::init(argc,argv,"rov_init_node");
@@ -25,18 +28,26 @@ int main(int argc,char **argv){
     int module_id;
 
     init_status status;
+    over_time=0;//进主循环后超时计时器开始计时，如果发现了有效自检就重置一次
     while(ros::ok()){
         rov_start_pkg::init_msg init_msg;//定义自检节点消息变量
         init_msg.module_name="center";//防止各个消息搞混，这是请求消息
         init_msg.command=true;
         init_msg.check_status=false;
+
+        
         if(!init_msg.check_status)
         {
             init_pub.publish(init_msg);//循环发送自检消息
         }
+        else{
+            ROS_INFO("Self-check have been done");
+            break;//check_status变为true时，输出提示并跳出主循环
+        }
+        
         ros::Subscriber init_sub=n_init.subscribe("init_check_re",10,init_callback);
 
         ROS_INFO("Init message has been published");
-
+        over_time++;
     }
 }
